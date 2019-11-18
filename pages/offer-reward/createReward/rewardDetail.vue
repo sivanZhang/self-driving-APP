aab8c798d5e41171dd04acaa6e5b8fb25ea58075
H1012 <1737403035@qq.com>
2019-11-18 23:19:03 +0800

    提供悬赏中删除unipop插件

--- pages/offer-reward/createReward/rewardDetail.vue
+++ pages/offer-reward/createReward/rewardDetail.vue
@@ -97,10 +97,9 @@
 
 <script>
 	import share from "@/common/share.js";
-	import uniPopup from '@/components/uni-popup/uni-popup.vue'
 	export default {
 		components: {
-			uniPopup
+			
 		},
 		data() {
 			return {
