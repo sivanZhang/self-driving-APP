94361053d88bd45536e0b29ee61241d22ed8badb
sunshine8866 <2837037335@qq.com>
2019-11-25 19:39:56 +0800

    车迹API注释修改

--- api/track.js
+++ api/track.js
@@ -1,13 +1,17 @@
 import Ajax from '../libs/http'
-//开启行程共享
+//开启车迹
 export function Track_Share(payload) {
     return Ajax.post('/car/open/', payload)
 };
-//记录轨迹
+//记录车迹
 export function Record_Track(payload) {
     return Ajax.post('/car/track/?put', payload)
 };
 //关闭车迹
 export function Close_Track(payload) {
     return Ajax.post('/car/open/?put', payload)
+};
+//返回车迹信息
+export function show_Track(payload) {
+    return Ajax.post('/car/track/', payload)
 };
