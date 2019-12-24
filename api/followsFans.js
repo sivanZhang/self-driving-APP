96b7a1bbd259b62016967a55fe4d1f1884370db8
H1012 <1737403035@qq.com>
2019-12-24 22:22:40 +0800

    添加粉丝关注API

---
+++ api/followsFans.js
@@ -0,0 +1,13 @@
+import Ajax from '../libs/http'
+//添加关注
+export function addConcern(data) {
+    return Ajax.post('/followfans/followfans/',data);
+}
+//取消关注
+export function DeleteConcern(data) {
+    return Ajax.post('/followfans/followfans/?delete',data);
+}
+//查看关注
+export function getConcern(params){
+	return Ajax.get('/followfans/followfans/',{params})
+}
