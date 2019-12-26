1e03367222015a0d7bbb37f475b3e65afa31b9ac
H1012 <1737403035@qq.com>
2019-12-26 21:37:51 +0800

    自己创建的路线添加可以删除功能

--- pages/team/releaseRouter/allRouterDetail.vue
+++ pages/team/releaseRouter/allRouterDetail.vue
@@ -4,18 +4,25 @@
 		<view style="padding: 15upx;">
 			<button type="primary" @tap="target('/pages/team/releaseRouter/releaseRouter')">临时发布路线按钮</button>
 		</view>
-		<block v-for="(item,index) of RouterList" :key="index" >
+		<block v-for="(item,index) of RouterList" :key="index">
 			<view class="container section">
 				<view class="detail">
 					<view class="content">
-						<view class="content-left">
+						<view class="content-top">
 							<image src="../../../static/image/face.jpg"></image>
 							<view class="content-middle">
 								<h4>{{item.creator_name}}</h4>
 								<p>发布日期:{{item.create_date|dateFormat}}</p>
 							</view>
-							<view @tap="deleteRouter(item.id)">
-								<image style="height:50upx;width: 50upx;" src="../../../static/icons/more.png"></image>
+							<view class="content-right">
+								<view class="content-right-follow" v-if="followClick" @tap="addConcern(item.id,index)">
+									<view style="font-size: 24upx;" class="iconfont icon-jiahao"></view>
+									<view>关注</view>
+								</view>
+								<view class="content-right-followover" v-else>
+									<view style="font-size: 24upx;" class="iconfont icon-duigou"></view>
+									<view>已关注</view>
+								</view>
 							</view>
 						</view>
 						<view class="place-list">
@@ -24,8 +31,7 @@
 									<view style="align-items: center;">
 										<view class="place-left" v-if="row.sort == -1">
 											<text>{{row.area_name}}</text>
-											<image style="width:80upx ;height: 60upx;"
-											src="../../../static/icons/youjiantou.png"></image>
+											<image style="width:80upx ;height: 60upx;" src="../../../static/icons/youjiantou.png"></image>
 										</view>
 										<view class="place-left" v-if="row.sort == 0">
 											<text>{{row.area_name}}</text>
@@ -34,7 +40,7 @@
 									<view v-if="show">
 										<text style="font-size: 22upx;">{{row.area_name}}-</text>
 									</view>
-									
+
 								</view>
 							</view>
 							<view class="place-point" @tap="placePoint">
@@ -42,73 +48,98 @@
 								<text>途经点</text>
 							</view>
 						</view>
-						<view class="time">
-								<span style="font-weight: bold;">时间安排：</span>
-								{{ item.start_date | dateFormat }}--{{ item.end_date | dateFormat }}
+						<view class="place-list">
+							<view>
+							<span style="font-weight: bold;">时间安排：</span>
+							{{ item.start_date | dateFormat }}--{{ item.end_date | dateFormat }}
+							</view>
+						    <view v-if="item.creator_id == UserInfo.id" @tap="deleteRouter(item.id)"><span style="color: #848689;">删除</span></view>
 						</view>
 					</view>
 				</view>
 				<view class="footer">
-					<view class="footer-left" @tap="target('/pages/team/createTeam/index')">组队</view>
+					<view class="footer-left" :style="{ color: activeColor}" @tap="target('/pages/team/createTeam/index')">组队</view>
 					<view class="footer-right">保存</view>
 				</view>
 			</view>
-			
+
 		</block>
 	</view>
 </template>
 
 <script>
-	import {queryRouterDetail,deleteRouter} from '@/api/router'	
-    // import uniSteps from "@/components/uni-steps/uni-steps.vue"
-	export default{
+	import {
+		queryRouterDetail,
+		deleteRouter
+	} from '@/api/router'
+	import {
+		addConcern
+	} from '@/api/followsFans'
+	export default {
 		data() {
 			return {
 				show: false,
-				RouterList:[],
+				RouterList: [],
+				followClick: true,
+				activeColor: '#fbb10e',
 			};
 		},
-		components: {
-			// uniSteps
+		computed: {
+			UserInfo() {
+				return this.$store.state.UserInfo
 			},
-		onLoad() {
-          this.getRouterList();
 		},
-		onShow(){
-		  this.getRouterList();
+		onShow() {
+			this.getRouterList();
 		},
-		methods:{
+		methods: {
+			//添加关注
+			addConcern(id,key) {
+				this.RouterList.forEach((item,index)=>{
+                  if(index == key){
+                   this.followClick = false;
+				addConcern({follow_ids:id}).then(({data})=>{
+				})}
+				})	
+			},
 			//删除路线
-			deleteRouter(id){
+			deleteRouter(id) {
 				var that = this;
 				uni.showModal({
 					content: '确定删除此路线吗？',
-					confirmColor:"#FF0000",
-				    success: function (res) {
-				        if (res.confirm) {
-							deleteRouter({method:"delete",ids:id}).then(({ data })=>{
-								if(data.status == 0){
+					confirmColor: "#FF0000",
+					success: function(res) {
+						if (res.confirm) {
+							deleteRouter({
+								method: "delete",
+								ids: id
+							}).then(({
+								data
+							}) => {
+								if (data.status == 0) {
 									uni.showToast({
-									    title: '删除成功',
-									    duration: 2000
+										title: '删除成功',
+										duration: 2000
 									});
 									that.getRouterList();
 								}
 							})
-				        } else if (res.cancel) {
-				            console.log('用户点击取消');
-				        }
-				    }
+						} else if (res.cancel) {
+							console.log('用户点击取消');
+						}
+					}
 				});
-				
+
 			},
 			//展示途经点
-			placePoint(){
+			placePoint() {
 				this.show = !this.show
 			},
 			//获取所有的路线列表
-			getRouterList(){
-				queryRouterDetail().then(({ data })=>{
+			getRouterList() {
+				queryRouterDetail().then(({
+					data
+				}) => {
 					this.RouterList = data.msg;
 				})
 			},
@@ -144,116 +175,151 @@
 </script>
 
 <style lang="scss">
-	#allRouterDetail{
-			// background-color: #efefef;
-			// height: 100vh;
-			// width: 100%;
-			.section{
-				padding-top: 25upx;
-				.detail {
-					// background-color: #FFFFFF;
+	#allRouterDetail {
+
+		// background-color: #efefef;
+		// height: 100vh;
+		// width: 100%;
+		.section {
+			padding-top: 25upx;
+
+			.detail {
+				// background-color: #FFFFFF;
+				display: flex;
+				justify-content: space-between;
+				padding: 20.833rpx;
+				min-height: 212.5upx;
+				border: 2.083upx solid #c8c8cc;
+				// border: 2.083upx solid #ececec;
+				// box-shadow: 0 6.25upx 6.25upx rgba(200, 200, 204, 0.8);
+				font-size: 27.083upx;
+
+				.content {
+					padding: 20.833rpx;
+					flex: 0 1 100%;
+					width: 100%;
+					font-size: 27.083upx;
+
+					.content-top {
 						display: flex;
 						justify-content: space-between;
-						padding: 20.833rpx;
-						min-height: 212.5upx;
-						border: 2.083upx solid #c8c8cc;
-						// border: 2.083upx solid #ececec;
-						// box-shadow: 0 6.25upx 6.25upx rgba(200, 200, 204, 0.8);
-						font-size: 27.083upx;
-				
-						.content {
-							padding: 20.833rpx;
-							flex: 0 1 100%;
-							width: 100%;
-							font-size: 27.083upx;
-							.content-left {
-								display: flex;
-								justify-content:space-between;
-								align-items: center;
-								& > image {
-									width: 90upx;
-									height: 90upx;
-									border-radius: 50%;
-								}
-							}
-							.content-middle {
-								padding-left: 15upx;
-							    -webkit-box-flex: 1;
-							    -webkit-flex: 1;
-							    flex: 1;
-							    min-width: 0;
-							}
-							
-							.content-middle h4 {
-							    font-weight: 400;
-							    font-size: 16px;
-							    width: auto;
-							    overflow: hidden;
-							    text-overflow: ellipsis;
-							    white-space: nowrap;
-							    word-wrap: normal;
-							    word-wrap: break-word;
-							    word-break: break-all;
-							    padding-bottom: 5px;
-							    color: #333;
-							}
-							
-							.content-middle p {
-							    color: #848689;
-							    font-size: 13px;
-							    line-height: 1.2;
-							    overflow: hidden;
-							    text-overflow: ellipsis;
-							    display: -webkit-box;
-							    -webkit-box-orient: vertical;
-							    -webkit-line-clamp: 1;
-							}
+						align-items: center;
+
+						&>image {
+							width: 90upx;
+							height: 90upx;
+							border-radius: 50%;
+						}
 					}
-					.place-list{
-						padding: 25upx 0upx;
-						display: flex;
-						justify-content: space-between;
+
+					.content-middle {
+						padding-left: 15upx;
+						-webkit-box-flex: 1;
+						-webkit-flex: 1;
+						flex: 1;
+						min-width: 0;
 					}
-					.place {
-						// width: 50%;
-						font-size: 40upx;
-						
+
+					.content-middle h4 {
+						font-weight: 400;
+						font-size: 16px;
+						width: auto;
+						overflow: hidden;
+						text-overflow: ellipsis;
+						white-space: nowrap;
+						word-wrap: normal;
+						word-wrap: break-word;
+						word-break: break-all;
+						padding-bottom: 5px;
+						color: #333;
 					}
-					.place-left {
-						display:flex;
-						align-items:center;
+
+					.content-middle p {
+						color: #848689;
+						font-size: 13px;
+						line-height: 1.2;
+						overflow: hidden;
+						text-overflow: ellipsis;
+						display: -webkit-box;
+						-webkit-box-orient: vertical;
+						-webkit-line-clamp: 1;
 					}
-					.place-point{
-							padding-left: 150upx;		
+
+					.content-right {
+						width: 20%;
+
+						.content-right-follow {
+							font-size: 24upx;
+							height: 20px;
+							display: flex;
+							justify-content: center;
+							align-items: center;
+							border-radius: 50px;
+							border: 1px solid #DF5000;
+							color: #DF5000
+						}
+
+						.content-right-followover {
+							font-size: 24upx;
+							height: 20px;
+							display: flex;
+							justify-content: center;
+							align-items: center;
+							border-radius: 50px;
+							border: 1px solid #c8c8cc;
+						}
 					}
 				}
-				.footer {
-					font-size: 32upx;
+
+				.place-list {
+					padding: 25upx 0upx;
+					display: flex;
+					justify-content: space-between;
+				}
+
+				.place {
+					// width: 50%;
+					font-size: 40upx;
+
+				}
+
+				.place-left {
 					display: flex;
 					align-items: center;
-					background-color:#fbfbfb;
-				
-					.footer-left {
-						display: flex;
-						justify-content: center;
-						padding: 30upx 20upx;
-						width: 50%;
-						border: 1px solid #c8c8cc;
-						border-top: 0px solid #c8c8cc;
-					}
-				
-					.footer-right {
-						display: flex;
-						justify-content: center;
-						padding: 30upx 20upx;
-						width: 50%;
-						border-bottom: 1px solid #c8c8cc;
-						// border-top: 1px solid #c8c8cc;
-						border-right: 1px solid #c8c8cc;
-					}
 				}
-				
+
+				.place-point {
+					padding-left: 150upx;
+				}
 			}
-			
+
+			.footer {
+				font-size: 32upx;
+				display: flex;
+				align-items: center;
+				background-color: #fbfbfb;
+
+				.footer-left {
+					display: flex;
+					justify-content: center;
+					padding: 30upx 20upx;
+					width: 50%;
+					border: 1px solid #c8c8cc;
+					border-top: 0px solid #c8c8cc;
+				}
+
+				.footer-right {
+					display: flex;
+					justify-content: center;
+					padding: 30upx 20upx;
+					width: 50%;
+					border-bottom: 1px solid #c8c8cc;
+					// border-top: 1px solid #c8c8cc;
+					border-right: 1px solid #c8c8cc;
+				}
+			}
+
+		}
+
 	}
 </style>
